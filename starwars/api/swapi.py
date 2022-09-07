626fdd8be140fa3278d3511f5961b39c04e07a5f
steven-sl <mla189@sfu.ca>
2022-09-07 00:08:01 -0700

    Link image URLs and fallback

--- starwars/api/swapi.py
+++ starwars/api/swapi.py
@@ -1,4 +1,5 @@
 import urllib.request, json 
+import os.path
 
 def get_sw_films():
     sw_films = urllib.request.urlopen("https://swapi.dev/api/films")
@@ -6,5 +7,17 @@ def get_sw_films():
     sw_films = json.loads(sw_films.read())
     sw_films = sw_films['results']
     sw_films.sort(key = lambda sw_films: sw_films['episode_id'], reverse = False)
+
+    # Add images
+    # print(os.path.exists('static/media/placeholder.jpg'))
+    # print(files)
+    for film in sw_films:
+        if os.path.exists("static/media/films/" + film['title'] + ".jpg"):
+            # link to downloaded images
+            film['img_url'] = "media/films/" + film['title'] + ".jpg"
+        else:
+            # link to failover image 
+            film['img_url'] = "media/fallback_poster.png"
+
 
     return sw_films
